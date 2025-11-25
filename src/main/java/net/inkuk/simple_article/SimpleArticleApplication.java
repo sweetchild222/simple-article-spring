d7ea3e59121d31951e01d1d54105995e619cf6e0
crazygun22 <crazygun22@nate.com>
2025-11-25 15:53:25 +0900

    update

---
+++ src/main/java/net/inkuk/simple_article/SimpleArticleApplication.java
@@ -0,0 +1,13 @@
+package net.inkuk.simple_article;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleArticleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleArticleApplication.class, args);
+	}
+
+}

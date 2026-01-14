54582851c30c55a3b57ae0fcb1c13ffc9ccb619e
inkuk <sweetchild22.ik@gmail.com>
2026-01-14 15:42:58 +0900

    update

---
+++ reference/server/run_remote.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export JAVA_HOME=/usr/lib/jvm/jdk-21
+export PATH=$JAVA_HOME/bin:$PATH
+kill $(ps aux | grep 'java -jar' | awk '{print $2}')
+java -jar ~/simple/simple-article-0.0.1.jar --spring.profiles.active=prod

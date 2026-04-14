b4d2cc8fca3bb65960d549430ab927b4676a7f2a
inkuk <sweetchild22.ik@gmail.com>
2026-04-14 10:01:30 +0900

    update

--- reference/server/run_remote.sh
+++ reference/server/run_remote.sh
@@ -3,4 +3,4 @@
 export JAVA_HOME=/usr/lib/jvm/jdk-21
 export PATH=$JAVA_HOME/bin:$PATH
 kill $(ps aux | grep 'java -jar' | awk '{print $2}')
-java -jar ~/simple/simple-article-0.0.1.jar --spring.profiles.active=prod
+java -jar ~/simple/simple-article-0.0.1.jar --enable-native-access=ALL-UNNAMED --spring.profiles.active=prod

bfb4f1db988e88bd1b0dc3425bb82d389d85c761
inkuk <sweetchild22.ik@gmail.com>
2026-09-07 15:42:39 +0900

    'update'

--- src/main/java/net/inkuk/simple_article/util/EMailService.java
+++ src/main/java/net/inkuk/simple_article/util/EMailService.java
@@ -24,7 +24,7 @@ public class EMailService {
 
             String body = "<h3>" + "요청하신 인증 번호입니다." + "</h3>";
             body += "<h1>인증 번호 : " + code + "</h1>";
-            body += "<h3>" + "인증 번호 유효 기간은 1시간 입니다. 감사합니다." + "</h3>";
+            body += "<h3>" + "인증 번호 유효 기간은 1시간 입니다. 감사합니다. by leafstory" + "</h3>";
 
             message.setRecipients(MimeMessage.RecipientType.TO, email);
             message.setSubject("이메일 인증");
@@ -69,7 +69,7 @@ public class EMailService {
 
             String body = "<h3>" + "임시 비밀번호가 발급되었습니다." + "</h3>";
             body += "<h1>임시 비밀 번호 : " + password + "</h1>";
-            body += "<h3>" + "감사합니다." + "</h3>";
+            body += "<h3>" + "감사합니다. by leafstory" + "</h3>";
 
             //message.setFrom(senderEmail);
             message.setRecipients(MimeMessage.RecipientType.TO, email);
